f2d3f61d29118c2bdd3644c15daf2c0fcf0503ea
Cajaite <145431034+Cajaite@users.noreply.github.com>
2023-09-24 21:58:02 +0200

    Add files via upload

---
+++ BOUCLE.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**            BOUCLE
+ *              FOR
+ * Declarer une variable r qui est egale à un entier positif
+ * 
+ * Faire un console log de 'reussi' autant de fois que le valeur de r
+ * 
+ * 
+ */
+
+let r=5;
+for(let i=0;i<r;i++){
+    console.log('reussi');
+}
